9bf602d08b42b702c227a653d10ecca8e359aa0e
Lidjan <minidmnv@gmail.com>
2018-12-20 20:44:16 +0100

    Commit inicjalny

---
+++ src/main/java/pl/badjuice/sivents/SiventsApplication.java
@@ -0,0 +1,14 @@
+package pl.badjuice.sivents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiventsApplication.class, args);
+	}
+
+}
+
